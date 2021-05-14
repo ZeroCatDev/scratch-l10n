b6479bf6b36632a18fadad8073509ec133663e04
Thomas Weber <weber_t@mailbox.org>
2021-05-13 19:28:01 -0500

    Update build script
     - Remove unused generated locales
     - use JSON.parse instead of object literal to optimize parsing, which can matter as this is 1.8MB of JSON

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -64,27 +64,38 @@ const combineJson = (component) => {
     }, {});
 };
 
+const stringify = messages =>
+    'JSON.parse(\'' +
+    JSON.stringify(messages)
+        .replace(/\\/g, '\\\\')
+        .replace(/'/g, '\\\'') +
+    '\')';
+
 // generate the blocks messages: files are plain key-value JSON
+/*
 let blocksMessages = combineJson('blocks');
 let blockData =
     '// GENERATED FILE:\n' +
     'export default ' +
-    JSON.stringify(blocksMessages, null, 2) +
+    stringify(blocksMessages) +
     ';\n';
 
 fs.writeFileSync(MSGS_DIR + 'blocks-msgs.js', blockData);
+*/
 
 // generate messages for gui components - all files are plain key-value JSON
 let components = ['interface', 'extensions', 'paint-editor'];
 let editorMsgs = {};
 components.forEach((component) => {
     let messages = combineJson(component);
+    /*
     let data =
         '// GENERATED FILE:\n' +
         'export default ' +
-        JSON.stringify(messages, null, 2) +
+        stringify(messages) +
         ';\n';
     fs.writeFileSync(MSGS_DIR + component + '-msgs.js', data);
+    */
     defaultsDeep(editorMsgs, messages);
 });
 
@@ -92,7 +103,7 @@ components.forEach((component) => {
 let editorData =
     '// GENERATED FILE:\n' +
     'export default ' +
-    JSON.stringify(editorMsgs, null, 2) +
+    stringify(editorMsgs) +
     ';\n';
 fs.writeFileSync(MSGS_DIR + 'editor-msgs.js', editorData);
 
