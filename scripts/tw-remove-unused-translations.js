15cc3f87b1f9ab6d4e4842b228151f1c8f2dc266
Thomas Weber <weber_t@mailbox.org>
2021-05-14 16:20:27 -0500

    Remove all unused translations
    2.1MB -> 1.4MB

--- scripts/tw-remove-unused-translations.js
+++ scripts/tw-remove-unused-translations.js
@@ -1,25 +1,9 @@
-const OMIT = [
-    /^gui\.howtos\./,
-    /^gui\.cards/,
-    /^gui\.loader\.message/,
-    /^gui\.comingSoon/,
-    'gui.gui.defaultProjectTitle',
-    'gui.tipsLibrary.tutorials',
-    'gui.crashMessage.label',
-    'gui.crashMessage.description',
-    'gui.webglModal.back',
-    'gui.webglModal.description',
-    'gui.webglModal.previewfaq',
-    'gui.webglModal.previewfaqlinktext'
-].map(i => {
-    if (typeof i === 'string') return new RegExp(`^${i.replace(/\./g, '\\.')}$`);
-    return i;
-});
+const strings = require('./tw-all-ids.json');
 
 const removeUnusedTranslations = langData => {
     let result = {};
     for (const key of Object.keys(langData)) {
-        if (!OMIT.some(i => i.test(key))) {
+        if (strings.includes(key)) {
             result[key] = langData[key];
         }
     }
